f00438e0e608b750f3b6a8cfa6e23b657bdaeaf6
roynatech2544 <69458352+roynatech2544@users.noreply.github.com>
2020-12-15 14:44:40 +0900

    Update get_files.sh

--- get_files.sh
+++ get_files.sh
@@ -4,6 +4,7 @@ curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
 chmod a+x ~/bin/repo
 git config --global user.name "John Doe"
 git config --global user.email johndoe@example.com
+cd ~
 mkdir TWRP
 cd TWRP
 yes | repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -c --no-tags --no-clone-bundle
