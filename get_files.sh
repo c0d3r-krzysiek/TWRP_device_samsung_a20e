1ad09def270993b79e722055c8193410856b96c4
roynatech2544 <69458352+roynatech2544@users.noreply.github.com>
2020-12-15 15:51:42 +0900

    Update get_files.sh

--- get_files.sh
+++ get_files.sh
@@ -9,3 +9,4 @@ mkdir TWRP
 cd TWRP
 yes | repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-10.0 -c --no-tags --no-clone-bundle
 repo sync > /dev/null
+yes | rm -r .repo
