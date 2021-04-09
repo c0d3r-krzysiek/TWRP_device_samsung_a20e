b3863364ced3ee1c6c68f19c94798428b9270466
roynatech2544 <69458352+roynatech2544@users.noreply.github.com>
2021-04-09 16:58:24 +0900

    Add cmdline

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,7 +22,7 @@ TARGET_PREBUILT_KERNEL := device/samsung/a20e/prebuilt/Image
 # Kernel Options
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
-
+BOARD_KERNEL_CMDLINE := androidboot.hardware=exynos7884B androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x10000000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_OFFSET := 0x00008000
